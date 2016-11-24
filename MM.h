4dc7ccf91df97454e475bc965e90c2b1d301c421
Eshed <ashdnazg@gmail.com>
2016-11-24 21:20:08 +0200

    add scenarios

--- MM.h
+++ MM.h
@@ -1,35 +1,39 @@
 #pragma once
 
-template <typename T, typename Cont, typename Comp>
-class PriorityQueue: public std::priority_queue<T, Cont, Comp> {
-public:
-	void clear() { this->c.clear(); }
-};
-
 template <class D, class H>
 class MMSearcher
 {
 public:
 	typedef typename D::NodeType DomainNode;
+	typedef typename D::CostType DomainCost;
+	typedef Neighbor<DomainNode, DomainCost> DomainNeighbor;
+
+	enum Direction {
+		FORWARD = 0,
+		BACK = 1
+	};
 
 	struct Node {
 		DomainNode domainNode;
-		int g;
-		int f;
+		DomainCost g;
+		DomainCost f;
+		Direction dir;
 	};
 
 	MMSearcher(const D* domain_) : expanded(0), generated(0), domain(domain_) { }
 	void reset(DomainNode start_, DomainNode goal_);
 	bool isEmpty() const;
 	Node expand();
-	void generate(DomainNode node, int distance);
-	void generate(std::vector<DomainNode>& nodesVec, int distance);
-	int search(DomainNode start, DomainNode goal);
-
+	void generate(DomainNode node, DomainCost distance, Direction dir);
+	void generate(std::vector<DomainNeighbor>& nodesVec, DomainCost distance, Direction dir);
+	DomainCost search(DomainNode start, DomainNode goal);
+	static inline bool compareNodes(const Node& x, const Node& y) {
+		return x.f == y.f ? (x.g < y.g) : (x.f > y.f);
+	}
 	// Functor for priority queue ordering
 	struct lessCost: public std::binary_function<const Node&, const Node&, bool> {
 		inline bool operator() (const Node& x, const Node& y) const {
-			return (x.f ==  y.f) ? (x.g < y.g) : (x.f > y.f);
+			return compareNodes(x, y);
 		}
 	};
 
@@ -37,39 +41,41 @@ public:
 	unsigned generated;
 private:
 	DomainNode goal;
-	typename D::List openList;
-	typename D::List closedList;
-	PriorityQueue<Node, std::vector<Node>, lessCost> q;
+	typename D::List openList[2];
+	typename D::List closedList[2];
+	PriorityQueue<Node, std::vector<Node>, lessCost> q[2];
 	const D* domain;
 };
 
 template <class D, class H>
-void MMSearcher<D,H>::reset(MMSearcher<D,H>::DomainNode start_, MMSearcher<D,H>::DomainNode goal_)
+void MMSearcher<D,H>::reset(DomainNode start_, DomainNode goal_)
 {
-	q.clear();
+	q[FORWARD].clear();
+	q[BACK].clear();
 	goal = goal_;
 
-	generate(start_, 0);
+	generate(start_, 0, FORWARD);
+	generate(goal_, 0, BACK);
 }
 
 template <class D, class H>
-void MMSearcher<D,H>::generate(MMSearcher<D,H>::DomainNode node, int distance)
+void MMSearcher<D,H>::generate(DomainNode node, DomainCost distance, Direction dir)
 {
-	if (openList.contains(node) || closedList.contains(node))
+	if (openList[dir].contains(node) || closedList[dir].contains(node))
 		return;
 
 	++generated;
-	openList.insert(node);
-	int h = H::get(domain, node, goal);
-	Node n = {node, distance, distance + h};
-	q.push(n);
+	openList[dir].insert(node);
+	DomainCost h = H::get(domain, node, goal);
+	Node n = {node, distance, distance + h, dir};
+	q[dir].push(n);
 }
 
 template <class D, class H>
-void MMSearcher<D,H>::generate(std::vector<MMSearcher<D,H>::DomainNode>& nodesVec, int distance)
+void MMSearcher<D,H>::generate(std::vector<DomainNeighbor>& nodesVec, DomainCost distance, Direction dir)
 {
-	for (const DomainNode& node: nodesVec) {
-		generate(node, distance);
+	for (const DomainNeighbor& n: nodesVec) {
+		generate(n.node, distance + n.cost, dir);
 	}
 }
 
@@ -77,27 +83,28 @@ template <class D, class H>
 typename MMSearcher<D,H>::Node MMSearcher<D,H>::expand()
 {
 	++expanded;
-	Node best = q.top();
-	q.pop();
-	closedList.insert(best.domainNode);
+	Direction d = (Direction) compareNodes(q[FORWARD].top(), q[BACK].top());
+	Node best = q[d].top();
+	q[d].pop();
+	closedList[d].insert(best.domainNode);
 	return best;
 }
 
 
 template<class D, class H>
-int MMSearcher<D,H>::search(MMSearcher<D,H>::DomainNode start_, MMSearcher<D,H>::DomainNode goal_) {
-	std::vector<DomainNode> tempNodes;
+typename MMSearcher<D,H>::DomainCost MMSearcher<D,H>::search(DomainNode start_, DomainNode goal_) {
+	std::vector<DomainNeighbor> tempNodes;
 	reset(start_, goal_);
 
-	while (!q.empty()) {
+	while (!q[FORWARD].empty() && !q[BACK].empty()) {
 		Node best = expand();
 
-		if (domain->same(best.domainNode, goal))
+		if (closedList[!best.dir].contains(best.domainNode))
 			return best.g;
 
 		tempNodes.clear();
 		domain->getNeighbors(best.domainNode, tempNodes);
-		generate(tempNodes, best.g + 1);
+		generate(tempNodes, best.g, best.dir);
 	}
 	return -1;
 }
--- Searcher.cpp
+++ Searcher.cpp
@@ -6,13 +6,15 @@
 #include "Domains.h"
 #include "BFS.h"
 #include "AStar.h"
+#include "MM.h"
 
 int main() {
 	//GridDomain domain(10,10);
 	OctileDomain domain(10,10);
 	//BFSSearcher<GridDomain> searcher(&domain);
 	//AStarSearcher<GridDomain, GridDomain::ManhattanDistance> searcher(&domain);
-	AStarSearcher<OctileDomain, OctileDomain::OctileDistance> searcher(&domain);
+	//AStarSearcher<OctileDomain, OctileDomain::OctileDistance> searcher(&domain);
+	MMSearcher<OctileDomain, OctileDomain::OctileDistance> searcher(&domain);
 	float res = searcher.search(0,58);
 	printf("%f %d %d\n", res, searcher.expanded, searcher.generated);
 	return 0;
